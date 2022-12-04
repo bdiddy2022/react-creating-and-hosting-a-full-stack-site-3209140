cdb545437127b74d1e7f43f21b0eddfa413d8e95
bdiddy2022 <brendan.t.downs@gmail.com>
2022-12-03 21:19:54 -0500

    01-07 Creating and Linking the Articles List --- Automated adding new articles to the articlesList page using .map and router-dom's Link function

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -7,12 +7,11 @@ const ArticlePage = () => {
     return (
         <>
         <h1>{article.title}</h1>
-        {article.content.map(paragraph => (
-            <p>{paragraph}</p>
+        {article.content.map((paragraph, i) => (
+            <p key={ i }>{ paragraph }</p>
             ))
         }
         </>
     );
 }
-
 export default ArticlePage;

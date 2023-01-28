4d9630bf66f4e7d7dc7609bdac23d20550f03c1e
bdiddy2022 <brendan.t.downs@gmail.com>
2023-01-28 17:35:14 -0500

    first attempt mongo host

--- my-blog-backend/src/server.js
+++ my-blog-backend/src/server.js
@@ -3,8 +3,10 @@ import admin from 'firebase-admin';
 import express from 'express';
 import { db, connectToDb } from './db.js';
 import path from 'path';
-
+import 'dotenv/config';
 import { fileURLToPath } from 'url';
+
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -23,7 +25,7 @@ app.use(express.static(path.join(__dirname, '../build')));
 
 app.get(/^(?!\/api).+/, (req, res) => {
     res.sendFile(path.join(__dirname, '../build/index.html'));
-})
+});
 
 app.use(async (req, res, next) => {
     const { authtoken } = req.headers;
